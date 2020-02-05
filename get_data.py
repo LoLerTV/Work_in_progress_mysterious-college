0f83379f92749dc02fe11e92044e33df889ccbbe
Vlad <lolertv@gmail.com>
2020-02-05 13:53:10 +0300

    Init

---
+++ get_data.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+#ввод: (комманда) (*объекты)
+# ввывод: к какой группе принадлежит команда
+
+# ввод> идти домой
+# выввод: go - комаанда, ["домой"] - объект
+
+from commands import commands
+
+
+input_data = input('>').split(' ')
+cmd = input_data[0]
+objct = input_data[1::]
+
+
+def find_cmd(**kwargs):
+    for key, value in kwargs.items():
+        if cmd in value:
+            return key
+        elif key == 'unknown command':
+            return f'Неизвестная команда: {cmd}'
+
+
+print(find_cmd(**commands))
