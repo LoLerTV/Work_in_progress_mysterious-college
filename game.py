ef533f7514325944edd575822327de030d0fc7da
Vlad <lolertv@gmail.com>
2020-03-02 23:23:55 +0300

    add Rooms and Player classes in game.py

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
+# -*- coding: utf-8 -*-
 
-import get_data
+
 from commands import commands
 import transitions
 
@@ -20,6 +21,7 @@ class Player:
         self.inventory = []
 
 
+
 scheduling= Room(transitions.scheduling_room[0],transitions.scheduling_room[1])
 
 
@@ -29,3 +31,50 @@ player = Player("Игрок", scheduling)
 #Start
 print("Таинственный колледж")
 print("\n")
+print("""Ох блин… Всё болит… Голова раскалывается… 
+Всё трясётся… Я ничего не вижу… Ох… Стоп, как я сюда попал? 
+И куда это сюда?""")
+
+
+while True:
+ 
+    #get command
+    # ввод: (комманда) (*объекты)
+    # ввывод: к какой группе принадлежит команда
+    # ввод: идти в дом
+    # выввод: go - команда(таким образом "идти" и "пойти" - одно и то же), ["в", "домой"] - объект
+
+    input_data = input('>').split(' ')
+    cmd = input_data[0].lower()
+    objct = input_data[1::]
+
+    
+    def find_cmd(**kwargs):     # Проверяет введённые данные и возвращает соотв. команду из commands.py 
+        for key, value in kwargs.items():
+            if cmd in value:
+                return key
+            elif key == 'unknown command':
+                return f'Неизвестная команда: {cmd}'
+
+
+    if find_cmd(**commands) == "unknown command":
+        print(find_cmd(**commands))
+
+
+    #Осмотреться
+    if find_cmd(**commands) == "look_around":
+        print('Это', player.location.name)
+        print(player.location.description)
+        print("Здесь есть: ")
+        for item in player.location.items:
+            print(item)
+        print("Отсюда можно пойти в: ")
+        for exit_in in player.location.exits:
+            print(exit_in)
+
+    
+
+
+
+    
+    
